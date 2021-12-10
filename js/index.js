286b9f76f4c3149143a12326826304ce9d1afed8
zenginerler <yavuzenginerler@gmail.com>
2021-12-09 18:53:38 -0600

    add go top button & organize index.js

--- js/index.js
+++ js/index.js
@@ -1,46 +1,69 @@
-//temporary js scripts
+// Main JavaScript Functionality
+(function () {
+  "use strict";
 
-// Handle resizing window
-var viewportHeight = window.innerHeight;
-var viewportWidth = window.innerWidth;
-window.addEventListener("resize", updateViewport);
-function updateViewport() {
-  viewportHeight = window.innerHeight;
-  viewportWidth = window.innerWidth;
+  // Global Variables
+  var viewportHeight = window.innerHeight;
+  var viewportWidth = window.innerWidth;
+  var prevScrollpos = window.pageYOffset;
 
-  //navigation bar adjustments
-  if (viewportWidth < 800){
-    document.getElementById("profile_pic1").style.display = "none";
-    document.getElementById("profile_pic2").style.display = "block";
-    document.getElementById("aboutMe_content_text").style.textAlign = "center";
-  } else {
-    document.getElementById("profile_pic1").style.display = "block";
-    document.getElementById("profile_pic2").style.display = "none";
-    document.getElementById("aboutMe_content_text").style.textAlign = "none";
+  // Hide-unhide navigation bar according to the scroll direction
+  // ------------------------------------------------------------
+  window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+
+    //color transition
+    var color = "transparent"
+    if (currentScrollPos > Math.max(viewportHeight, 800)) {
+      color = "#0a0a0a"
+    } else {
+      color = "transparent"
+    }
+
+    if (prevScrollpos > currentScrollPos) { //scrolling up
+      document.getElementById("bannerScroll").style.top = "0";
+      document.getElementById("bannerScroll").style.backgroundColor = color;
+    } else { //scrolling down
+      document.getElementById("bannerScroll").style.top = "-60px";
+      document.getElementById("bannerScroll").style.backgroundColor = color;
+    }
+    prevScrollpos = currentScrollPos;
   }
-}
 
 
-// Hide-unhide navigation bar according to the scroll direction
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
+  // Handle resizing window
+  // ------------------------------------------------------------
+  function updateViewport() {
+    viewportHeight = window.innerHeight;
+    viewportWidth = window.innerWidth;
 
-  //color transition
-  var color = "transparent"
-  if (currentScrollPos > Math.max(viewportHeight, 800)){
-    color = "#0a0a0a"
-  }else {
-    color = "transparent"
+    //navigation bar adjustments
+    if (viewportWidth < 800) {
+      document.getElementById("profile_pic1").style.display = "none";
+      document.getElementById("profile_pic2").style.display = "block";
+      document.getElementById("aboutMe_content_text").style.textAlign = "center";
+    } else {
+      document.getElementById("profile_pic1").style.display = "block";
+      document.getElementById("profile_pic2").style.display = "none";
+      document.getElementById("aboutMe_content_text").style.textAlign = "none";
+    }
   }
-  
-  if (prevScrollpos > currentScrollPos) { //scrolling up
-    document.getElementById("bannerScroll").style.top = "0";
-    document.getElementById("bannerScroll").style.backgroundColor = color;
-  } else { //scrolling down
-    document.getElementById("bannerScroll").style.top = "-60px";
-    document.getElementById("bannerScroll").style.backgroundColor = color;
-  }
-  prevScrollpos = currentScrollPos;
-}
+  window.addEventListener("resize", updateViewport);
 
+
+  // Handle go to the top of the page button
+  // ------------------------------------------------------------
+  function showUpButton() {
+    const upButtonPx = 700;
+    const goTopButton = document.querySelector(".go-top-button");
+
+    // Show/hide the button
+    if (window.scrollY >= upButtonPx) {
+      if (!goTopButton.classList.contains('button-is-visible')) goTopButton.classList.add("button-is-visible")
+    } else {
+      goTopButton.classList.remove("button-is-visible")
+    }
+  }
+  window.addEventListener('scroll', showUpButton);
+
+})();
